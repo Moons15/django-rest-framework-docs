67603fccef51278fe59c4c7f6cd7fb410d4de39a
Richard Cancino <richard.cancino@pucp.pe>
2018-06-18 02:25:02 -0500

    first commit

---
+++ demo/project/organisations/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from project.organisations import views
+
+app_name = "organisations"
+
+urlpatterns = [
+
+    url(r'^create/$', view=views.CreateOrganisationView.as_view(), name="create"),
+    url(r'^(?P<slug>[\w-]+)/$', view=views.RetrieveOrganisationView.as_view(), name="organisation"),
+    url(r'^(?P<slug>[\w-]+)/members/$', view=views.OrganisationMembersView.as_view(), name="members"),
+    url(r'^(?P<slug>[\w-]+)/leave/$', view=views.LeaveOrganisationView.as_view(), name="leave")
+
+]
